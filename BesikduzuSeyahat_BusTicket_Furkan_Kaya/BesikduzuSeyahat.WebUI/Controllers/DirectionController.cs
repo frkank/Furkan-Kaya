c22b55da825a0a787b9d01fcb020f105c77e8e09
frkank <wissenfurkankaya1@gmail.com>
2022-06-23 14:58:47 +0300

    23.06.2022

---
+++ BesikduzuSeyahat_BusTicket_Furkan_Kaya/BesikduzuSeyahat.WebUI/Controllers/DirectionController.cs
@@ -0,0 +1,43 @@
+﻿using BesikduzuSeyahat.Business.Abstract;
+using BesikduzuSeyahat.Entity;
+using BesikduzuSeyahat.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BesikduzuSeyahat.WebUI.Controllers
+{
+    public class DirectionController : Controller
+    {
+        private readonly IDirectionService _directionService;
+        private readonly ICityService _cityService;
+
+        public DirectionController(IDirectionService directionService, ICityService cityService)
+        {
+            _directionService = directionService;
+            _cityService = cityService;
+        }
+
+        public IActionResult DirectionList()
+        {
+            var getDirections = _directionService.GetAll();
+            return View(getDirections);
+        }
+        public IActionResult CreateDirection()
+        {
+            ViewBag.Cities = new SelectList(_cityService.GetAll().OrderBy(i => i.CityName), "CityName", "CityName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateDirection(Direction direction)
+        {
+            _directionService.Create(direction);
+            ViewBag.Cities = new SelectList(_cityService.GetAll().OrderBy(i => i.CityName), "CityName", "CityName");
+            return View();
+        }
+        
+    }
+}
---
+++ BesikduzuSeyahat_BusTicket_Furkan_Kaya/BesikduzuSeyahat.WebUI/Models/DirectionCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BesikduzuSeyahat.WebUI.Models
+{
+    public class DirectionCreateViewModel
+    {
+        public string Start { get; set; }
+        public string direction1 { get; set; }
+        public string direction2 { get; set; }
+        public string direction3 { get; set; }
+        public string Finish { get; set; }
+
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        public double Price { get; set; }
+        public SelectList CitySelectList { get; set; }
+    }
+}
